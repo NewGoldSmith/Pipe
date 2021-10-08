abd6df0df053261b80ccb8b9f62c5a5ef7ec3710
S.Y <70691930+NewGoldSmith@users.noreply.github.com>
2021-10-09 01:17:50 +0900

    Add files via upload

--- PopeProbe/resource.h
+++ PopeProbe/resource.h
@@ -2,8 +2,13 @@
 // Microsoft Visual C++ で生成されたインクルード ファイル。
 // PopeProbe.rc で使用
 //
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
 #define IDD_POPEPROBE_DIALOG            102
+#define COPY_RIGHT                      102
 #define IDR_MAINFRAME                   128
+#define IDR_MENU_TASK_MENU              131
 #define IDC_LIST_MAIN                   1000
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON_GO_TASKTRAY          1001
@@ -14,14 +19,16 @@
 #define IDC_EDIT_MAIN                   1005
 #define ID_PIPE_DISCONNECT              1006
 #define IDC_STATIC_INFOMATION           1007
+#define ID_TASK_MENU_CLOSE              32772
+#define ID__ABOUTWCMDCLIANT             32773
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1008
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
